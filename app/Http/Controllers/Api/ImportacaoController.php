aa9f14aec35a1d78d63d6cf5623de8289a5cb4a3
omartins-zs <gabrielmatheus209@gmail.com>
2025-01-14 17:15:15 -0300

    Ajuste no Importação Controller para Generico

--- app/Http/Controllers/Api/ImportacaoController.php
+++ app/Http/Controllers/Api/ImportacaoController.php
@@ -13,21 +13,25 @@ class ImportacaoController extends Controller
     {
         // Logando a requisição para verificar os cabeçalhos e o que foi recebido
         Log::info('Requisição recebida na API do Projeto 2', ['headers' => $request->headers->all()]);
+        Log::debug('Dados recebidos', ['request_data' => $request->all()]);
 
         try {
             // Validando se o arquivo foi enviado e é do tipo correto
             $request->validate([
                 'arquivo' => 'required|file|mimes:csv,xlsx,xls,txt|max:10240', // Permitindo também arquivos .txt, se necessário
+                'tabela'  => 'required|string', // Validando que a tabela foi enviada
             ]);
 
             $file = $request->file('arquivo');
+            $tabela = $request->input('tabela');
 
             // Logando o nome do arquivo recebido
             Log::info('Arquivo recebido', ['nome_original' => $file->getClientOriginalName()]);
-
             // Preparando o arquivo para ser enviado ao próximo serviço (se necessário)
             $response = Http::attach('arquivo', file_get_contents($file), 'arquivo.' . $file->getClientOriginalExtension())
-                ->post('http://127.0.0.1:8003/api/processar-arquivo');
+                ->post("http://127.0.0.1:8003/api/importar/{$tabela}", [
+                    'tabela' => $tabela
+                ]);
 
             // Verificando a resposta do Projeto 3
             if ($response->successful()) {
